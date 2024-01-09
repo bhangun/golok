38b7d75b86907a1533bfc58384f0a7cd701a6abb
Bhangun <raden.hartani@ist.id>
2024-01-09 23:15:20 +0700

    parse entities

--- core/utils.js
+++ core/utils.js
@@ -2,31 +2,80 @@ import process from 'node:process';
 import  { dirname, resolve, join  } from 'node:path';
 import { readFileSync, writeFile, readdirSync, writeFileSync, mkdir } from 'node:fs';
 import { Buffer } from 'node:buffer';
+import { exec, spawn } from 'node:child_process';
+import Crayon from './crayon.js';
 import ejs from 'ejs';
 import YAML from 'yaml';
+
+
 const TEMPLATE_DIR =  './template/flutter/riverpod';
 const MODE_0666 = parseInt('0666', 8)
 const MODE_0755 = parseInt('0755', 8)
 
 export { parse, rewriteFile}
 
+
+function grey(text){
+  var t = '\x1b[2m%s\x1b[0m';
+  return text;
+}
 /**
  * 
  */
 function parse(path, destination){
-    const file = readFileSync(path, 'utf8')
-    const context = YAML.parse(file);
+    const file = readFileSync(path, 'utf8');
+    const context = parseYaml(file);
 
     //process.env.PATH
     //console.log(dirname('../flutter'));
-
+    //exec('"../coba1/flutter create coba2" arg1 arg2')
     //console.log(path.dirname('../flutter'));
+    //initiateFlutter();
 
-    copyTemplateMulti('', destination, context);
+    //copyTemplateMulti('', destination, context);
     //makeDir(destination);
     //template('/Users/bhangun/workopen/golok/data/template.ejs',ym);
 
 } 
+
+function parseYaml(file){
+  var context = YAML.parse(file);
+  var entities = parseProperties(context.entities);
+  context.entities = entities;
+  return context;
+}
+
+/**
+ * Check an parse the properties, especialy default properties and type to string
+ */
+function parseProperties(entities){
+  entities.forEach(el => {
+    el.properties.forEach((prop, i) => {
+      // If the array is string, treated as default string properties
+      if(typeCheck(prop) == 'string'){
+        el.properties[i] = {
+          name: prop,
+          type: 'string'
+        }
+      }
+      // If the array type is not defined, treated as default string properties
+      if(!prop.type) el.properties[i].type = 'string'
+    });
+  });
+  return entities;
+}
+
+function initiateFlutter(){
+  //spawn('"flutter"', ['create', '../coba3'], { shell: true });
+
+  exec('"flutter" create coba4', (err, stdout, stderr) => {
+    if (err) {
+      console.error(err);
+      return;
+    }
+    console.log(stdout);
+  });
+}
 
 /**
  * Copy multiple files from template directory.
@@ -113,3 +162,22 @@ function makeDir(dir){
     console.log('Directory created successfully!'); 
   }); 
 }
+
+
+/**
+    array
+    date
+    string
+    boolean
+    null 
+    object
+*/
+const typeCheck = (value) => {
+  const return_value = Object.prototype.toString.call(value);
+  const type = return_value.substring(
+  return_value.indexOf(" ") + 1,
+  return_value.indexOf("]")
+  );
+  return type.toLowerCase();
+};
+
