556dff93566030b4a135a693dfe6face32b5d834
Bhangun <raden.hartani@ist.id>
2024-01-14 16:53:03 +0700

    add entity tempalte

--- index.js
+++ index.js
@@ -3,12 +3,11 @@
 import { Command } from 'commander';
 import { generate } from'./lib/utils.js';
 import Logo  from './lib/logo.js';
+//import packagejs from './package.json' with { type: "json" };
 
-const program = new Command();
-                                                                                                     
-                                                            
+const program = new Command();                                                                                                                                              
 program.name('golok')
-  .description('CLI to some JavaScript string utilities')
+  .description('Generate application by model definition.')
   .version('0.1.0');
 
 Logo.show();
