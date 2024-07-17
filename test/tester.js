3a9bd919aad65d9f30043a3ce0902afe7e4d0771
Bhangun <raden.hartani@ist.id>
2024-07-17 10:40:16 +0700

    fixing enum

--- test/tester.js
+++ test/tester.js
@@ -1,7 +1,15 @@
-import {Enums, Properties} from '../lib/core/blueprint.js';
+//import {Enums, Properties} from '../lib/core/blueprint.js';
 
-const en = new Enums();
+/* const en = new Enums();
 en.setEnums(['g', 'kg', 'ee', 'gg', 'vv']);
 const prop = new Properties([{'ini': 'saja'}, {'no': 3}]);
 // console.log(en.getModel());
-console.log(prop.getObject());
+console.log(prop.getObject()); */
+
+const yaml = {
+    aa : '',
+    bb: [{'kk':['ff','gg']}, {'mm':['faf','gga']}]
+}
+Object.entries(yaml.bb).forEach(el => {
+    console.log(el)
+});
