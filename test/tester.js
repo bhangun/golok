ed8a771a249cfe04a35d8f6f53958c381b35550a
Bhangun <raden.hartani@ist.id>
2024-07-22 10:55:25 +0700

    add dt, service, paginate

--- test/tester.js
+++ test/tester.js
@@ -7,7 +7,7 @@ const prop = new Properties([{'ini': 'saja'}, {'no': 3}]);
 console.log(prop.getObject()); */
 
 ///////////////
-const _relationship = [
+/* const _relationship = [
     {'type': 'many', 'name':'aa'},
     {'type': 'many', 'name':'aa'},
     {'type': 'one', 'name':'bb'},
@@ -55,7 +55,7 @@ function groupBy(input, key) {
 //const groupByType = _relationship.reduce(groupBy, 'type')
 
 //console.log(groupBy(_relationship, 'type'));
-console.log(groupBy2( 'type'));
+console.log(groupBy2( 'type')); */
 /////////////
 
 /* const yaml = {
@@ -102,3 +102,50 @@ function grouping(total, currentValue, currentIndex, arr) {
 
 //console.log(groupBy('type'));
 
+//////////////
+
+var str = "10M255S"
+//var match = str.match(/^([0-9]*)M*([0-9]*)S*$/)
+
+var str = "* with ServiceImpl"
+var str2 = "Product,status with ServiceImpl"
+var str3= "String, min=3, max=30, required //Product code"
+var match = str.match(/^(.[A-z]*.)with*.([A-z]*)$/)
+var match2 = str2.match(/^(.[A-z].*.)with*.([A-z]*)$/)
+var string = str2.match(/^(.[A-z].*.)with*.([A-z]*)$/)
+
+console.log(match)
+console.log(match2)
+
+
+/*
+// Character classes
+.	any character except newline
+\w\d\s	word, digit, whitespace
+\W\D\S	not word, digit, whitespace
+[abc]	any of a, b, or c
+[^abc]	not a, b, or c
+[a-g]	character between a & g
+
+// Anchors
+^abc$	start / end of the string
+\b\B	word, not-word boundary
+
+// Escaped characters
+\.\*\\	escaped special characters
+\t\n\r	tab, linefeed, carriage return
+
+// Groups & Lookaround
+(abc)	capture group
+\1	backreference to group #1
+(?:abc)	non-capturing group
+(?=abc)	positive lookahead
+(?!abc)	negative lookahead
+
+// Quantifiers & Alternation
+a*a+a?	0 or more, 1 or more, 0 or 1
+a{5}a{2,}	exactly five, two or more
+a{1,3}	between one & three
+a+?a{2,}?	match as few as possible
+ab|cd	match ab or cd
+*/
