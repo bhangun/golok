b1f93c71bc9fe7414bded7f0a5b7859190f0cba4
bhangun hartani <bhangunh@gmail.com>
2024-11-02 20:39:30 +0700

    fixing ai

--- golok/golok/lib/app/pages/main_page.dart
+++
@@ -1,5 +0,0 @@
-import '../../core/modules/menu.dart';
-import '../../core/modules/modules_registry.dart';
-import '../layout/large_screen.dart';
-import '../layout/medium_screen.dart';
-import '../layout/phone_screen.dart';
